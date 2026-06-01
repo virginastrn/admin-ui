c96e7d75865ae2eda99c864bda677381ebed2cc3
virgina <virgina@example.com>
2026-06-01 12:21:16 +0700

    menambahkan file vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import "./App.css";
 import SignInPage from "./pages/signIn";
 import SignUpPage from "./pages/signUp";
+import ErrorPage from "./pages/error";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function App() {
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: (				
+      <div className="flex justify-center items-center min-h-screen">
+          <Link to="/login" className="p-2 m-5 bg-primary text-white">
+            Login
+          </Link>
+          |
+          <Link to="/register" className="p-2 m-5 bg-primary text-white">
+            Register
+          </Link>
+        </div>
+      ),
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/login",
+      element: <SignInPage />,
+    },
+    {
+      path: "/register",
+      element: <SignUpPage />,
+    },
+  ]);
+
   return (
     <>
-      <SignUpPage />
+      <RouterProvider router={myRouter} />
     </>
   );
 }
