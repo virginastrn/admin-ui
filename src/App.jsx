dfcc568058ac2e3b8e24511f5ba389cfb4e8d59c
virgina <virgina@example.com>
2026-06-02 21:37:04 +0700

    Responsive Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import SignInPage from "./pages/signIn";
 import SignUpPage from "./pages/signUp";
 import ErrorPage from "./pages/error";
+import DashboardPage from "./pages/dashboard";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -9,17 +10,7 @@ function App() {
   const myRouter = createBrowserRouter([
     {
       path: "/",
-      element: (				
-      <div className="flex justify-center items-center min-h-screen">
-          <Link to="/login" className="p-2 m-5 bg-primary text-white">
-            Login
-          </Link>
-          |
-          <Link to="/register" className="p-2 m-5 bg-primary text-white">
-            Register
-          </Link>
-        </div>
-      ),
+      element: <DashboardPage />,
       errorElement: <ErrorPage />,
     },
     {
